53b9af7691555d408c3982b5294ec05b74eea020
jasmine Magdi <jesojolay@yahoo.com>
2017-09-09 13:28:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,31 +10,31 @@ function destructivelyPrependKitten(name) {
   kittens.unshift(name);
   return kittens;
 }
-//
-// function destructivelyRemoveLastKitten() {
-//    return kittens.pop();
-// }
-//
-// function destructivelyRemoveFirstKitten() {
-//   return kittens.shift();
-// }
-//
-// function appendKitten(name) {
-//   var newarr = kittens[...kittens, name];
-//   return newarr;
-// }
-//
-// function prependKitten(name) {
-//   var newarr = kittens[name, ...kittens];
-//   return newarr;
-// }
-//
-// function removeLastKitten() {
-//   var newarr = kittens.slice(0, kittens - 1);
-//   return newarr;
-// }
-//
-// function removeFirstKitten() {
-//     var newarr = kittens.slice(1);
-//     return newarr;
-// }
+
+function destructivelyRemoveLastKitten() {
+   return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  var newarr = kittens[...kittens, name];
+  return newarr;
+}
+
+function prependKitten(name) {
+  var newarr = kittens[name, ...kittens];
+  return newarr;
+}
+
+function removeLastKitten() {
+  var newarr = kittens.slice(0, kittens - 1);
+  return newarr;
+}
+
+function removeFirstKitten() {
+    var newarr = kittens.slice(1);
+    return newarr;
+}
