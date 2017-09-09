f0199843e70e54794f18e6e0b5fa62d83b42b2e9
jasmine Magdi <jesojolay@yahoo.com>
2017-09-09 13:29:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,32 +10,32 @@ function destructivelyPrependKitten(name) {
   kittens.unshift(name);
   return kittens;
 }
-// 
+//
 // function destructivelyRemoveLastKitten() {
 //     kittens.pop();
 //     return kittens;
 // }
-// 
+//
 // function destructivelyRemoveFirstKitten() {
 //    kittens.shift();
 //    return kittens;
 // }
-// 
+//
 // function appendKitten(name) {
 //   var newarr = kittens[...kittens, name];
 //   return newarr;
 // }
-// 
+//
 // function prependKitten(name) {
 //   var newarr = kittens[name, ...kittens];
 //   return newarr;
 // }
-// 
+//
 // function removeLastKitten() {
 //   var newarr = kittens.slice(0, kittens - 1);
 //   return newarr;
 // }
-// 
+//
 // function removeFirstKitten() {
 //     var newarr = kittens.slice(1);
 //     return newarr;
