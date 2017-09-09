a235d36d213692c938425fb5d5a6bd974e012108
jasmine Magdi <jesojolay@yahoo.com>
2017-09-09 13:25:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name) {
-  kittens.push(name);
+  return kittens.push(name);
 }
 
 function destructivelyPrependKitten(name)() {
-  kittens.unshift(name);
+  return kittens.unshift(name);
 }
 
 function destructivelyRemoveLastKitten() {
-  kittens.pop();
+   return kittens.pop();
 }
 
 function destructivelyRemoveFirstKitten() {
-  kittens.shift();
+  return kittens.shift();
 }
 
 function appendKitten(name) {
